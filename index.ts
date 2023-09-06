3a21e26660965c7bac3f8e1dfda27abe7472d172
Mateusz Bruzda <mateusz.bruzda@nokia.com>
2023-09-06 21:10:20 +0200

    Added end points from ADV week5 day4

--- index.ts
+++ index.ts
@@ -3,26 +3,19 @@ import cors from 'cors';
 import 'express-async-errors';
 import { ValidationError, handleError } from "./utils/errors";
 import rateLimit from 'express-rate-limit';
+import { adRouter } from "./routers/ad.router";
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
-// app.use(express.json());
 app.use(rateLimit({
   windowMs: 5 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 }))
 
-app.use(json());
-
-//Routes
-
-app.get('/', async(req, res) => {
-    // throw new Error('Daaam!');
-    throw new ValidationError('Daaam!');
-});
+app.use('/ad', adRouter);
 
 app.use(handleError)
 
